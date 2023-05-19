16eb7570b3571b330be3d5d0d03b784a8624312f
angelrodriguez26 <avargas@gobetterfly.com>
2023-05-19 14:33:18 -0500

    second

---
+++ src/app/openai.service.ts
@@ -0,0 +1,11 @@
+
+// export class OpenaiService {
+//
+//   private openAi;
+//
+//   constructor() {
+//     if(!open)
+//   }
+//
+//
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,65 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai';
+import { useEffect, useState } from 'react';
+import { AxiosResponse } from 'axios';
 
-export default function Home() {
+const Home = () => {
+
+  const [text, setText] = useState<string>('');
+  const [completionText, setCompletionText] = useState<string>('');
+
+  const getOpenAi = () => {
+    const configuration = new Configuration({
+      apiKey: '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'
+    });
+
+    return new OpenAIApi(configuration);
+  }
+
+
+  const getCompletion = async () => {
+    const openAi = getOpenAi();
+
+    // const completion = await openAi.createCompletion(
+    //   {
+    //     model: "text-davinci-003",
+    //     prompt: text,
+    //   }
+    // );
+
+    const completion = await openAi.createChatCompletion(
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": "Who won the world series in 2020?"},
+          {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+          {"role": "user", "content": "Where was it played?"}
+        ]
+      }
+    ) as AxiosResponse<CreateChatCompletionResponse>;
+
+    console.log(completion)
+
+    setCompletionText(completion.data.choices[0].message.content);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <div>fake gpt</div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <input onChange={(e) => { setText(e.target.value)}} value={text}/>
+      <button onClick={getCompletion}>completar</button>
+      <div>
+        {completionText}
       </div>
     </main>
+
+
+
   )
 }
+
+export default Home
